5e443977c1b6949c636cd1da5494151815815025
devlin <73556235+mandaloriandeveloper@users.noreply.github.com>
2020-11-12 16:00:35 +0100

    upgraded to angular 11

--- src/app/features/hero/containers/hero-detail/hero-detail.component.spec.ts
+++ src/app/features/hero/containers/hero-detail/hero-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HeroDetailComponent } from './hero-detail.component';
 
@@ -6,7 +6,7 @@ describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HeroDetailComponent ]
     })
--- src/app/features/villain/containers/villain-detail/villain-detail.component.spec.ts
+++ src/app/features/villain/containers/villain-detail/villain-detail.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { VillainDetailComponent } from './villain-detail.component';
 
@@ -6,7 +6,7 @@ describe('VillainDetailComponent', () => {
   let component: VillainDetailComponent;
   let fixture: ComponentFixture<VillainDetailComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ VillainDetailComponent ]
     })
