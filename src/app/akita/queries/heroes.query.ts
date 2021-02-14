53ccf090b3144f1dc576fa70f6330e4d4e72b2d4
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-02-14 15:48:51 +0100

    updated

--- src/app/akita/queries/heroes.query.ts
+++ src/app/akita/queries/heroes.query.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { QueryEntity } from "@datorama/akita";
-import { map } from "rxjs/operators";
 import { HeroesState, HeroModel } from "../../features/hero/hero.model";
 import { HeroesStore } from "../stores/heroes.store";
 
