89d98acbbf64fb321fb9d33b1f240da499d7e09a
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-02-19 19:34:36 +0100

    updated

--- src/app/akita/services/villains.service.ts
+++ src/app/akita/services/villains.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { environment } from "../../../environments/environment";
+import { environment } from "src/environments/environment";
 import { HttpClient } from "@angular/common/http";
 import { ID, transaction } from "@datorama/akita";
 import { VillainsStore } from "../stores/villains.store";
